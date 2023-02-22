8a8ec9ee92c2774f162d0a32ebd61ab72d599d70
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2023-02-22 10:48:33 +0100

    Add vp8 to tests

--- tests/videocodecs.js
+++ tests/videocodecs.js
@@ -26,40 +26,41 @@ tap.test("VideoCodecs",async function(suite){
 		//Test for ok
 		test.end();
 	});
-	await suite.test("encoder+decoder", async function(test){
-		try {
-			//Create encoder and decoder
-			const videoEncoder	= VideoCodecs.createVideoEncoder("h264", {
-				width	: 640,
-				height	: 380,
-				fps	: 30,
-				bitrate : 512000
-			});
-			const videoDecoder	= VideoCodecs.createVideoDecoder();
-			//Attach
-			videoEncoder.attachTo(videoDecoder);
-			//Create video track
-			const videoTrack = videoEncoder.createIncomingStreamTrack(); 
-			//Attach decoder to encoder video track
-			videoDecoder.attachTo(videoTrack);
+	for (const codec of ["h264","vp8"])
+		await suite.test("encoder+decoder " + codec, async function(test){
+			try {
+				//Create encoder and decoder
+				const videoEncoder	= VideoCodecs.createVideoEncoder("h264", {
+					width	: 640,
+					height	: 380,
+					fps	: 30,
+					bitrate : 512000
+				});
+				const videoDecoder	= VideoCodecs.createVideoDecoder();
+				//Attach
+				videoEncoder.attachTo(videoDecoder);
+				//Create video track
+				const videoTrack = videoEncoder.createIncomingStreamTrack(); 
+				//Attach decoder to encoder video track
+				videoDecoder.attachTo(videoTrack);
 
-			await sleep(100)
+				await sleep(100)
 
-			//Detach
-			videoEncoder.detach();
-			videoDecoder.detach();
+				//Detach
+				videoEncoder.detach();
+				videoDecoder.detach();
 			
-			//Stop all
-			videoEncoder.stop();
-			videoDecoder.stop();
-			//Test for ok
-			test.end();
-		} catch (error) {
-			console.error(error);
-			//Should not get here
-			test.fail();
-		}
-	});
+				//Stop all
+				videoEncoder.stop();
+				videoDecoder.stop();
+				//Test for ok
+				test.end();
+			} catch (error) {
+				console.error(error);
+				//Should not get here
+				test.fail();
+			}
+		});
 	suite.end();
 }).then(() =>
 {
